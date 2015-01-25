c2bf5ad83e3935000d80385035b83f4e71c20251
emilylowder <emilylowder@192.168.2.4>
2015-01-24 17:45:36 -0700

    Added code to Player.java

--- src/battleship/Player.java
+++ src/battleship/Player.java
@@ -5,10 +5,44 @@
  */
 package battleship;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Debbi
+ * @author Emily
  */
 public class Player {
+    
+    String name1;
+    String name2;
+    String welcome = "Welcome to Battleship! You will be playing against \n"
+            +"the computer. Get ready, and good luck!";
+    
+    
+    public static void main(String[] args) {
+        Battleship myGame = new Battleship();
+        myGame.getName1();
+        myGame.getName2();
+        // TODO code application logic here
+    }
+    
+    public void getName1()   {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Player 1 name: ");
+        this.name1 = input.next();
+            
+        }
+    
+    public void getName2()  {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Computer name");
+        this.name2 = input.next();
+        
+    }
+    
+        public void displayHelp()   {
+        System.out.println("Hello " + this.name1 + "\n");
+        System.out.println(this.welcome);
+    }
     
 }
