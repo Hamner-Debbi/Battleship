97491dad80d9938ae5f67c1393b205a0bbf21a27
Debbi <Debbi@192.168.0.3>
2015-02-07 23:22:36 -0700

    L04 Individual Programming Assignment - Deb

---
+++ src/battleship/GameMenuControl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author Debbi
+ */
+public class GameMenuControl {
+
+    private Game game;
+    private Board board;
+
+    public GameMenuControl(Game game) {
+        this.game = game;
+        this.board = game.board;
+    }
+  
+   public void takeTurn() {
+        System.out.println("\n\ttakeTurn() called");
+    }
+          
+   public void displayBoard() {
+        System.out.println("\n\tdisplayBoard() called");
+    }
+   
+    public void startNewGame() {
+        System.out.println("\n\tstartNewGame() called");
+    }     
+            
+    public void displayPreferencesMenu() {
+        GamePreferencesMenuView gamePreferencesMenu = new GamePreferencesMenuView(this.game);
+        gamePreferencesMenu.getInput();
+    }
+    
+    public void displayStatistics() {
+        String NamedPlayerStatistics = this.game.playerA.getPlayerStastics();
+        String ComputerStatistics = this.game.playerB.getPlayerStastics();
+        System.out.println("\t " + NamedPlayerStatistics);
+        System.out.println("\n\t " + ComputerStatistics);
+    }
+    
+    public void displayHelpMenu() {
+        HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.getInput();
+    }
+}
---
+++ src/battleship/GameMenuView.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+import java.util.Scanner;
+
+/**
+ * @author Debbi
+ */
+public class GameMenuView {
+    
+    private Game game;
+    private GameMenuControl gameMenuControl ; 
+
+    private final static String[][] menuItems = {
+        {"T", "Take your turn"},
+        {"D", "Display the board"},
+        {"N", "New Game"},
+        {"R", "Report stastics"},
+        {"P", "Change game preferences"},
+        {"H", "Help"},
+        {"Q", "QUIT"}
+    };
+
+    public GameMenuView(Game game) {
+        this.gameMenuControl = new GameMenuControl(game); 
+    }
+
+    public void getInput() {
+   
+        String command;
+        Scanner inFile = new Scanner(System.in);
+
+        do {    
+            this.display(); // display the menu
+
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "T":
+                    this.gameMenuControl.takeTurn();
+                    break;
+                case "D":
+                    gameMenuControl.displayBoard();
+                    break;
+                case "N":
+                    gameMenuControl.startNewGame();
+                    break;
+                case "R":
+                    gameMenuControl.displayStatistics();
+                    break;
+                case "P":
+                    gameMenuControl.displayPreferencesMenu();
+                    break;
+                case "H":
+                    gameMenuControl.displayHelpMenu();
+                    break;
+                case "Q":                   
+                    break;
+                default: 
+                    new BattleshipError().displayError("Invalid command. Please enter a valid command.");
+                    continue;                              
+            }
+        } while (!command.equals("Q"));
+
+        return;
+    }
+    
+    public final void display() {
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < GameMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        
+  
+}
+}
+
