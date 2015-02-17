88e66b80b3468f8e7ad2ba1d0682b89afc78b98f
emilylowder <emilylowder@192.168.2.4>
2015-02-16 22:45:56 -0700

    Adding GetPlayerScores

---
+++ src/battleship/GetPlayerScores.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author King Aaron, Lord of Pancakes and stuff
+ */
+public class GetPlayerScores {
+    
+    public static void ExchangeSort ( int [ ] num )
+{
+  int scores[] = {1, 2, 3, 100, 5, 7, 11, 13, 17, 19, 23};  
+  int i, j, temp; //be sure that the temp variable is the same "type" as the array
+  for ( i = 0; i < num.length - 1; i ++ )  
+  {
+  for ( j = i + 1; j < num.length; j ++ )
+  {
+  if( num[ i ] < num[ j ] ) //sorting into descending order
+  {
+  temp = num[ i ]; //swapping
+  num[ i ] = num[ j ];
+  num[ j ] = temp; 
+  }  
+  }
+  }
+}
+}
