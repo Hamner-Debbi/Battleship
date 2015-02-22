b1b0c2d49157cb3731c39fe7410acd5f0a7887e6
Debbi <Debbi@192.168.0.3>
2015-02-21 23:09:09 -0700

    L06 Individual Programming Assignment sorting Array info included

--- Battleship/src/battleship/GameMenuControl.java
+++ Battleship/src/battleship/GameMenuControl.java
@@ -59,7 +59,7 @@ public class Sort{
   }
   
    public static void main(String[] args) {
-    int [] array = {5,3,0,2,4,1,0,5,2,3,1,4}; 
+    int [] array = {5,3,0,2,4,1}; 
     System.out.println("Before: " + Arrays.toString(array));
     sort(array);
     System.out.println("After:  " + Arrays.toString(array));
