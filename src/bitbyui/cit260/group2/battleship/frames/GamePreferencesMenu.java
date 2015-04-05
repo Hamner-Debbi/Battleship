9bc0247149a32a6c35e220fc8107c446c05bb5ab
Taylor-Aaron <bluaeon@gmail.com>
2015-04-04 20:48:41 -0700

    L12 GamePreferencesMenu Placeholder

---
+++ src/bitbyui/cit260/group2/battleship/frames/GamePreferencesMenu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitbyui.cit260.group2.battleship.frames;
+
+/**
+ *
+ * @author King Aaron
+ * 
+ * PLACEHOLDER
+ * 
+ * We may not end up needing a preferences window. Probably not, but in case I will leave this here so I remember about it. 
+ * 
+ * 
+ */
+public class GamePreferencesMenu {
+    
+}
